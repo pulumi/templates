0409e76fb48cefb6652e22d41b754ab7ffbdbb5c
Fraser Waters <frassle@gmail.com>
2018-08-15 13:47:44 +0100

    Openstack Typescript template

---
+++ openstack-typescript/index.ts
@@ -0,0 +1,11 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as os from "@pulumi/openstack";
+
+// Create an OpenStack resource (Compute Instance)
+const instance = new os.compute.Instance("test", {
+	flavorName: "s1-2",
+	imageName: "Ubuntu 16.04",
+});
+
+// Export the IP of the instance
+export const instanceIP = instance.accessIpV4;
