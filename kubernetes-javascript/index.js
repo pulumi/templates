8a499900485536e9ae1a65b4d1573ea5bebbe4ae
Justin Van Patten <jvp@justinvp.com>
2018-08-29 09:47:37 -0700

    Add minimal Kubernetes JavaScript template
    Based on the TypeScript template

---
+++ kubernetes-javascript/index.js
@@ -0,0 +1,14 @@
+"use strict";
+const k8s = require("@pulumi/kubernetes");
+
+const appLabels = { app: "nginx" };
+const deployment = new k8s.apps.v1beta1.Deployment("nginx", {
+    spec: {
+        selector: { matchLabels: appLabels },
+        replicas: 1,
+        template: {
+            metadata: { labels: appLabels },
+            spec: { containers: [{ name: "nginx", image: "nginx" }] }
+        }
+    }
+});
