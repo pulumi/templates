f40f78a36d4a8abc07e791da56d99a3525701f7b
Ming Fang <mingfang@mac.com>
2018-09-02 15:00:11 -0400

    Upgrade from Kubernetes v1beta1 to v1

--- kubernetes-typescript/index.ts
+++ kubernetes-typescript/index.ts
@@ -1,7 +1,7 @@
 import * as k8s from "@pulumi/kubernetes";
 
 const appLabels = { app: "nginx" };
-const deployment = new k8s.apps.v1beta1.Deployment("nginx", {
+const deployment = new k8s.apps.v1.Deployment("nginx", {
     spec: {
         selector: { matchLabels: appLabels },
         replicas: 1,
