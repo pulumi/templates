9db002c85e0b9749be4e836a6fd599518ba6bc9a
Cyrus Najmabadi <cyrus@pulumi.com>
2018-10-05 11:32:32 -0700

    Update template.

--- hello-aws-javascript/index.js
+++ hello-aws-javascript/index.js
@@ -1,14 +1,30 @@
-// Import the [cloud-aws](https://pulumi.io/packages/pulumi-cloud/) package
-const cloud = require("@pulumi/cloud-aws");
+// Import the [pulumi/aws](https://pulumi.io/reference/pkg/nodejs/@pulumi/aws/index.html) package
+const aws = require("@pulumi/aws");
 
 // Create a public HTTP endpoint (using AWS APIGateway)
-const endpoint = new cloud.API("hello");
+const endpoint = new aws.apigateway.x.API("hello", {
+    routes: [
+        // Serve static files from the `www` folder (using AWS S3)
+        {
+            path: "/",
+            localPath: "www",
+        },
 
-// Serve static files from the `www` folder (using AWS S3)
-endpoint.static("/", "www");
-
-// Serve a simple REST API on `GET /name` (using AWS Lambda)
-endpoint.get("/source", (req, res) => res.json({name: "AWS"}));
+        // Serve a simple REST API on `GET /name` (using AWS Lambda)
+        {
+            path: "/source",
+            method: "GET",
+            eventHandler: (req, ctx, cb) => {
+                cb(undefined, {
+                    statusCode: 200,
+                    body: Buffer.from(JSON.stringify({ name: "AWS" }), "utf8").toString("base64"),
+                    isBase64Encoded: true,
+                    headers: { "content-type": "application/json" },
+                });
+            },
+        },
+    ],
+});
 
 // Export the public URL for the HTTP service
-exports.url = endpoint.publish().url;
+exports.url = endpoint.url;
