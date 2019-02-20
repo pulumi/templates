6cc73415e1d2b69623d0b8a4b330d5e09b93ab1d
Justin Van Patten <jvp@justinvp.com>
2019-02-20 15:50:55 -0800

    Add kubernetes-python template

---
+++ kubernetes-python/__main__.py
@@ -0,0 +1,16 @@
+import pulumi
+from pulumi_kubernetes.apps.v1 import Deployment
+
+app_labels = { "app": "nginx" }
+
+deployment = Deployment("nginx",
+    spec={
+        "selector": { "match_labels": app_labels },
+        "replicas": 1,
+        "template": {
+            "metadata": { "labels": app_labels },
+            "spec": { "containers": [{ "name": "nginx", "image": "nginx" }] }
+        }
+    })
+
+pulumi.export("name", deployment.metadata.apply(lambda m: m.name))
