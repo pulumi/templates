9430b52e3fb849ed8f9ad1467a0d9dade525c609
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-04-08 12:56:11 -0700

    Update template to include awsx

--- aws-typescript/index.ts
+++ aws-typescript/index.ts
@@ -1,5 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
+import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
