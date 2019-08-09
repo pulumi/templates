cb677a94131e05a0253f2d27265b8330a2c02d4d
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-08-09 14:50:54 -0700

    Set azure:location in the Pulumi config. Remove location from the storage account resource in the Azure templates.

--- azure-javascript/index.js
+++ azure-javascript/index.js
@@ -3,12 +3,11 @@ const pulumi = require("@pulumi/pulumi");
 const azure = require("@pulumi/azure");
 
 // Create an Azure Resource Group
-const resourceGroup = new azure.core.ResourceGroup("resourceGroup", {
-    location: "WestUS",
-});
+const resourceGroup = new azure.core.ResourceGroup("resourceGroup");
 
 // Create an Azure resource (Storage Account)
 const account = new azure.storage.Account("storage", {
+    // The location for the storage account will be derived automatically from the resource group.
     resourceGroupName: resourceGroup.name,
     accountTier: "Standard",
     accountReplicationType: "LRS",
