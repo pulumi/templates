e122f7fcdf44da8b2b227386d65a475f2086f399
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2019-08-13 12:59:20 -0700

    Set azure:location in the Pulumi config for the Azure templates (#60)
    * Set azure:location in the Pulumi config. Remove location from the storage account resource in the Azure templates.

--- azure-python/__main__.py
+++ azure-python/__main__.py
@@ -2,14 +2,14 @@ import pulumi
 from pulumi_azure import core, storage
 
 # Create an Azure Resource Group
-resource_group = core.ResourceGroup("resource_group", 
-    location='WestUS')
+resource_group = core.ResourceGroup('resource_group')
 
 # Create an Azure resource (Storage Account)
-account = storage.Account("storage", 
-    resource_group_name=resource_group.name,
-    account_tier='Standard',
-    account_replication_type='LRS')
+account = storage.Account('storage',
+                          # The location for the storage account will be derived automatically from the resource group.
+                          resource_group_name=resource_group.name,
+                          account_tier='Standard',
+                          account_replication_type='LRS')
 
 # Export the connection string for the storage account
 pulumi.export('connection_string', account.primary_connection_string)
