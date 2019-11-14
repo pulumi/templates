5c5285b41c1f56188d225602d6547d57eb523e19
Levi Blackstone <levi@pulumi.com>
2019-11-14 11:55:34 -0700

    Remove kx code from template and just include the import

--- kubernetes-typescript/index.ts
+++ kubernetes-typescript/index.ts
@@ -1,7 +1,6 @@
 import * as k8s from "@pulumi/kubernetes";
 import * as kx from "@pulumi/kubernetesx";
 
-// Define and create a Kubernetes Deployment.
 const appLabels = { app: "nginx" };
 const deployment = new k8s.apps.v1.Deployment("nginx", {
     spec: {
@@ -14,14 +13,3 @@ const deployment = new k8s.apps.v1.Deployment("nginx", {
     }
 });
 export const name = deployment.metadata.name;
-
-//
-// This is equivalent code using the kx package.
-//
-const pb = new kx.PodBuilder({
-    containers: [{ name: "nginx", image: "nginx" }]
-});
-const deploymentKx = new kx.Deployment("nginx-kx", {
-    spec: pb.asDeploymentSpec({ replicas: 1 }),
-});
-export const nameKx = deploymentKx.metadata.name;
