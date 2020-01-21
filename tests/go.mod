2fefd2cfe5389878762dc2a43b60593bc3e805f7
stack72 <public@paulstack.co.uk>
2020-01-21 21:42:38 +0200

    Adding ability to run tests against the templates

---
+++ tests/go.mod
@@ -0,0 +1,10 @@
+module github.com/pulumi/templates
+
+go 1.13
+
+require (
+	github.com/pulumi/pulumi v1.9.1
+	github.com/stretchr/testify v1.4.1-0.20191106224347-f1bd0923b832
+)
+
+replace github.com/Azure/go-autorest => github.com/Azure/go-autorest v12.4.3+incompatible
---
+++ tests/template_test.go
@@ -0,0 +1,115 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	ptesting "github.com/pulumi/pulumi/pkg/testing"
+	"github.com/pulumi/pulumi/pkg/testing/integration"
+	"github.com/pulumi/pulumi/pkg/workspace"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTemplates(t *testing.T) {
+	awsRegion := os.Getenv("AWS_REGION")
+	if awsRegion == "" {
+		awsRegion = "us-west-1"
+		fmt.Println("Defaulting AWS_REGION to 'us-west-1'.  You can override using the AWS_REGION environment variable")
+	}
+	//azureEnviron := os.Getenv("ARM_ENVIRONMENT")
+	//if azureEnviron == "" {
+	//	azureEnviron = "public"
+	//	fmt.Println("Defaulting ARM_ENVIRONMENT to 'public'.  You can override using the ARM_ENVIRONMENT variable")
+	//}
+	//azureLocation := os.Getenv("ARM_LOCATION")
+	//if azureLocation == "" {
+	//	azureLocation = "westus"
+	//	fmt.Println("Defaulting ARM_LOCATION to 'westus'.  You can override using the ARM_LOCATION variable")
+	//}
+	//gcpProject := os.Getenv("GOOGLE_PROJECT")
+	//if gcpProject == "" {
+	//	gcpProject = "pulumi-ci-gcp-provider"
+	//	fmt.Println("Defaulting GOOGLE_PROJECT to 'pulumi-ci-gcp-provider'." +
+	//		"You can override using the GOOGLE_PROJECT variable")
+	//}
+	//gcpRegion := os.Getenv("GOOGLE_REGION")
+	//if gcpRegion == "" {
+	//	gcpRegion = "us-central1"
+	//	fmt.Println("Defaulting GOOGLE_REGION to 'us-central1'.  You can override using the GOOGLE_REGION variable")
+	//}
+	//gcpZone := os.Getenv("GOOGLE_ZONE")
+	//if gcpZone == "" {
+	//	gcpZone = "us-central1-a"
+	//	fmt.Println("Defaulting GOOGLE_ZONE to 'us-central1-a'.  You can override using the GOOGLE_ZONE variable")
+	//}
+	overrides, err := integration.DecodeMapString(os.Getenv("PULUMI_TEST_NODE_OVERRIDES"))
+	if !assert.NoError(t, err, "expected valid override map: %v", err) {
+		return
+	}
+
+	base := integration.ProgramTestOptions{
+		Tracing:              "https://tracing.pulumi-engineering.com/collector/api/v1/spans",
+		ExpectRefreshChanges: true,
+		Overrides:            overrides,
+		Quick:                true,
+		SkipRefresh:          true,
+		NoParallel:           true, // we mark tests as Parallel manually when instantiating
+	}
+
+	// Retrieve the template repo.
+	repo, err := workspace.RetrieveTemplates("", false /*offline*/, workspace.TemplateKindPulumiProject)
+	assert.NoError(t, err)
+	defer assert.NoError(t, repo.Delete())
+
+	// List the templates from the repo.
+	templates, err := repo.Templates()
+	assert.NoError(t, err)
+
+	for _, template := range templates {
+		templateName := template.Name
+		t.Run(templateName, func(t *testing.T) {
+			t.Parallel()
+			if !strings.Contains(templateName, "aws") || strings.Contains(templateName, "go") {
+				t.Skipf("Skipping template test %s", templateName)
+				return
+			}
+
+			e := ptesting.NewEnvironment(t)
+			defer deleteIfNotFailed(e)
+
+			e.RunCommand("pulumi", "new", templateName, "-f", "-s", "template-test")
+
+			path, err := workspace.DetectProjectPathFrom(e.RootPath)
+			assert.NoError(t, err)
+			assert.NotEmpty(t, path)
+
+			_, err = workspace.LoadProject(path)
+			assert.NoError(t, err)
+
+			example := base.With(integration.ProgramTestOptions{
+				Dir: e.RootPath,
+				Config: map[string]string{
+					"aws:region": awsRegion,
+					//"azure:environment": azureEnviron,
+					//"azure:location":    azureLocation,
+					//"gcp:project":       gcpProject,
+					//"gcp:region":        gcpRegion,
+					//"gcp:zone":          gcpZone,
+					//"cloud:provider": "aws",
+				},
+			})
+
+			integration.ProgramTest(t, &example)
+		})
+	}
+}
+
+// deleteIfNotFailed deletes the files in the testing environment if the testcase has
+// not failed. (Otherwise they are left to aid debugging.)
+func deleteIfNotFailed(e *ptesting.Environment) {
+	if !e.T.Failed() {
+		e.DeleteEnvironment()
+	}
+}
