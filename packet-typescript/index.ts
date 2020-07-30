2a41835011f8c11ae676236026d3b1866c550f71
David McKay <david@rawkode.com>
2020-07-30 16:40:15 +0100

    fix: use better name for project resource and export its name (#128)

--- packet-typescript/index.ts
+++ packet-typescript/index.ts
@@ -2,9 +2,9 @@ import * as pulumi from "@pulumi/pulumi";
 import * as packet from "@pulumi/packet";
 
 // Create a Packet resource (Project)
-const domain = new packet.Project("my-test-project", {
-    name: "My Test Project"
+const project = new packet.Project("my-test-project", {
+  name: "My Test Project",
 });
 
 // Export the name of the project
-export const projectName = packet.name;
+export const projectName = project.name;
