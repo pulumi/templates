b1b26c8ee21e67e3c3227b16f4812dbfe55983d5
evanboyle <evan@pulumi.com>
2020-10-14 13:10:01 -0700

    automation api go template

---
+++ automation-api-go/go.mod
@@ -0,0 +1,7 @@
+module ${PROJECT}
+
+go 1.14
+
+require (
+	github.com/pulumi/pulumi/sdk/v2 v2.12.0
+)
---
+++ automation-api-go/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v2/go/x/auto"
+	"github.com/pulumi/pulumi/sdk/v2/go/x/auto/optdestroy"
+	"github.com/pulumi/pulumi/sdk/v2/go/x/auto/optup"
+)
+
+func main() {
+	// to destroy our program, we can run `go run main.go destroy`
+	destroy := false
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) > 0 {
+		if argsWithoutProg[0] == "destroy" {
+			destroy = true
+		}
+	}
+
+	// define our program that creates our pulumi resources.
+	// we refer to this style as "inline" pulumi programs where both program + automation can be compiled in the same
+	// binary. no need for sepearate projects.
+	deployFunc := func(ctx *pulumi.Context) error {
+
+		// this is where we define our pulumi deployment and create resources.
+		// in this "hello world", we just have a static stack export.
+
+		ctx.Export("hello", pulumi.String("world"))
+		return nil
+	}
+
+	ctx := context.Background()
+
+	projectName := "${PROJECT}"
+	// we use a simple stack name here, but recommend using auto.FullyQualifiedStackName for maximum specificity.
+	stackName := "dev"
+	// stackName := auto.FullyQualifiedStackName("myOrgOrUser", projectName, stackName)
+
+	// create or select a stack matching the specified name and project.
+	// this will set up a workspace with everything necessary to run our inline program (deployFunc)
+	s, err := auto.UpsertStackInlineSource(ctx, stackName, projectName, deployFunc)
+
+	fmt.Printf("Created/Selected stack %q\n", stackName)
+
+	w := s.Workspace()
+
+	fmt.Println("Installing the AWS plugin")
+
+	// for inline source programs, we must manage plugins ourselves
+	err = w.InstallPlugin(ctx, "aws", "v3.2.1")
+	if err != nil {
+		fmt.Printf("Failed to install program plugins: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Successfully installed AWS plugin")
+
+	// set stack configuration specifying the AWS region to deploy
+	s.SetConfig(ctx, "aws:region", auto.ConfigValue{Value: "us-west-2"})
+
+	fmt.Println("Successfully set config")
+	fmt.Println("Starting refresh")
+
+	_, err = s.Refresh(ctx)
+	if err != nil {
+		fmt.Printf("Failed to refresh stack: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Refresh succeeded!")
+
+	if destroy {
+		fmt.Println("Starting stack destroy")
+
+		// wire up our destroy to stream progress to stdout
+		stdoutStreamer := optdestroy.ProgressStreams(os.Stdout)
+
+		// destroy our stack and exit early
+		_, err := s.Destroy(ctx, stdoutStreamer)
+		if err != nil {
+			fmt.Printf("Failed to destroy stack: %v", err)
+		}
+		fmt.Println("Stack successfully destroyed")
+		os.Exit(0)
+	}
+
+	fmt.Println("Starting update")
+
+	// wire up our update to stream progress to stdout
+	stdoutStreamer := optup.ProgressStreams(os.Stdout)
+
+	// run the update to deploy our s3 website
+	res, err := s.Up(ctx, stdoutStreamer)
+	if err != nil {
+		fmt.Printf("Failed to update stack: %v\n\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Update succeeded!")
+
+	// get the URL from the stack outputs
+	url, ok := res.Outputs["websiteUrl"].Value.(string)
+	if !ok {
+		fmt.Println("Failed to unmarshall output URL")
+		os.Exit(1)
+	}
+
+	fmt.Printf("URL: %s\n", url)
+}
