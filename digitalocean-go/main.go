fcc454a68f150c79e88d67ea1ffbc254b04ca080
stack72 <public@paulstack.co.uk>
2020-10-26 13:42:44 +0000

    Bump DigitalOcean templates to v3.0.0

--- digitalocean-go/main.go
+++ digitalocean-go/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-digitalocean/sdk/v2/go/digitalocean"
+	"github.com/pulumi/pulumi-digitalocean/sdk/v3/go/digitalocean"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
