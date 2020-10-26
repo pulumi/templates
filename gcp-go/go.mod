ca906f5ac1d9ffe8708eccc93d9dc25458b8593d
stack72 <public@paulstack.co.uk>
2020-10-26 22:55:47 +0000

    Fixing up the Gcp Go template

--- gcp-go/go.mod
+++ gcp-go/go.mod
@@ -3,6 +3,6 @@ module ${PROJECT}
 go 1.14
 
 require (
-	github.com/pulumi/pulumi-gcp/sdk/v3 v4.0.0
+	github.com/pulumi/pulumi-gcp/sdk/v4 v4.0.0
 	github.com/pulumi/pulumi/sdk/v2 v2.9.2
 )
--- gcp-go/main.go
+++ gcp-go/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-gcp/sdk/v3/go/gcp/storage"
+	"github.com/pulumi/pulumi-gcp/sdk/v4/go/gcp/storage"
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
