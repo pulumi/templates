1188d4ff652648351c01c0065043c53cf96b0a06
Anton Tayanovskyy <anton@pulumi.com>
2021-06-24 17:57:43 -0400

    Fix compilation

--- tests/performance_test.go
+++ tests/performance_test.go
@@ -12,8 +12,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/pulumi/pulumi-trace-tool/traces"
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/workspace"
