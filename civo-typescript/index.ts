29d99871c9d14aa886b83fb02098c8ab8005456c
Engin Diri <engin.diri@mail.schwarz>
2022-04-22 00:40:47 +0200

    feat: remove breaking changes in demo code for Civo Kubernetes. (#266)

--- civo-typescript/index.ts
+++ civo-typescript/index.ts
@@ -2,17 +2,19 @@ import * as pulumi from "@pulumi/pulumi";
 import * as civo from "@pulumi/civo";
 
 const firewall = new civo.Firewall("civo-firewall", {
-    name: "myFirstFirewall",
-    region: "LON1",
-    createDefaultRules: true,
+  name: "myFirstFirewall",
+  region: "LON1",
+  createDefaultRules: true,
 });
 
 const cluster = new civo.KubernetesCluster("civo-k3s-cluster", {
-    name: "myFirstCivoCluster",
-    numTargetNodes: 3,
-    targetNodesSize: "g3.k3s.medium",
-    region: "LON1",
-    firewallId: firewall.id,
+  name: "myFirstCivoCluster",
+  pools: {
+    nodeCount: 3,
+    size: "g4s.kube.medium"
+  },
+  region: "LON1",
+  firewallId: firewall.id,
 })
 
 export const clusterName = cluster.name
