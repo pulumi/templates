3025a050feb57ae0cfb692bd63613e84512f9304
Luke Hoban <luke@pulumi.com>
2022-09-19 21:39:49 -0700

    Add initial set of architecture templates (#318)

---
+++ serverless-aws-typescript/index.ts
@@ -0,0 +1,24 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import * as apigateway from "@pulumi/aws-apigateway";
+
+// A Lambda function to invoke
+const fn = new aws.lambda.CallbackFunction("fn", {
+    callback: async (ev, ctx) => {
+        return {
+            statusCode: 200,
+            body: new Date().toISOString(),
+        };
+    }
+})
+
+// A REST API to route requests to HTML content and the Lambda function
+const api = new apigateway.RestAPI("api", {
+    routes: [
+        { path: "/", localPath: "www"},
+        { path: "/date", method: "GET", eventHandler: fn },
+    ]
+});
+
+// The URL at which the REST API will be served.
+export const url = api.url;
