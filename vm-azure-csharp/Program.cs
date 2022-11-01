050a03ae8768335f32918a282a53d747cbbb861c
Christian Nunciato <christian@pulumi.com>
2022-11-01 11:06:04 -0700

    Add Azure VM templates (#429)

---
+++ vm-azure-csharp/Program.cs
@@ -0,0 +1,187 @@
+﻿using Pulumi;
+using AzureNative = Pulumi.AzureNative;
+using Random = Pulumi.Random;
+using System.Collections.Generic;
+using System;
+using System.Text;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    // Import the program's configuration settings.
+    var config = new Pulumi.Config();
+    var vmName = config.Get("vmName") ?? "my-server";
+    var vmSize = config.Get("vmSize") ?? "Standard_A1_v2";
+    var osImage = config.Get("osImage") ?? "Debian:debian-11:11:latest";
+    var adminUsername = config.Get("adminUsername") ?? "pulumiuser";
+    var servicePort = config.Get("servicePort") ?? "80";
+    var sshPublicKey = config.Require("sshPublicKey");
+
+    string[] osImageArgs = osImage.Split(":");
+    var osImagePublisher = osImageArgs[0];
+    var osImageOffer = osImageArgs[1];
+    var osImageSku = osImageArgs[2];
+    var osImageVersion = osImageArgs[3];
+
+    // Create a resource group.
+    var resourceGroup = new AzureNative.Resources.ResourceGroup("resource-group");
+
+    // Create a virtual network.
+    var virtualNetwork = new AzureNative.Network.VirtualNetwork("network", new()
+    {
+        ResourceGroupName = resourceGroup.Name,
+        AddressSpace = new AzureNative.Network.Inputs.AddressSpaceArgs {
+            AddressPrefixes = new[]
+            {
+                "10.0.0.0/16",
+            },
+        },
+        Subnets = new[] {
+            new AzureNative.Network.Inputs.SubnetArgs {
+                Name = $"{vmName}-subnet",
+                AddressPrefix = "10.0.1.0/24",
+            },
+        },
+    });
+
+    // Use a random string to give the VM a unique DNS name.
+    var domainNameLabel = new Random.RandomString("domain-label", new()
+    {
+        Length = 8,
+        Upper= false,
+        Special = false,
+    }).Result.Apply(result => $"{vmName}-{result}");
+
+    // Create a public IP address for the VM.
+    var publicIp = new AzureNative.Network.PublicIPAddress("public-ip", new()
+    {
+        ResourceGroupName = resourceGroup.Name,
+        PublicIPAllocationMethod = AzureNative.Network.IPAllocationMethod.Dynamic,
+        DnsSettings = new AzureNative.Network.Inputs.PublicIPAddressDnsSettingsArgs {
+            DomainNameLabel = domainNameLabel,
+        },
+    });
+
+    // Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH).
+    var securityGroup = new AzureNative.Network.NetworkSecurityGroup("security-group", new()
+    {
+        ResourceGroupName = resourceGroup.Name,
+        SecurityRules = new[]
+        {
+            new AzureNative.Network.Inputs.SecurityRuleArgs {
+                Name = $"{vmName}-securityrule",
+                Priority = 1000,
+                Direction = AzureNative.Network.SecurityRuleDirection.Inbound,
+                Access = "Allow",
+                Protocol = "Tcp",
+                SourcePortRange = "*",
+                SourceAddressPrefix = "*",
+                DestinationAddressPrefix = "*",
+                DestinationPortRanges = new[]
+                {
+                    servicePort,
+                    "22",
+                },
+            },
+        },
+    });
+
+    // Create a network interface with the virtual network, IP address, and security group.
+    var networkInterface = new AzureNative.Network.NetworkInterface("network-interface", new()
+    {
+        ResourceGroupName = resourceGroup.Name,
+        NetworkSecurityGroup = new AzureNative.Network.Inputs.NetworkSecurityGroupArgs {
+            Id = securityGroup.Id
+        },
+        IpConfigurations = new[]
+        {
+            new AzureNative.Network.Inputs.NetworkInterfaceIPConfigurationArgs {
+                Name = $"{vmName}-ipconfiguration",
+                PrivateIPAllocationMethod = AzureNative.Network.IPAllocationMethod.Dynamic,
+                Subnet = new AzureNative.Network.Inputs.SubnetArgs {
+                    Id = virtualNetwork.Subnets.GetAt(0).Apply(subnet => subnet.Id!)
+                },
+                PublicIPAddress = new AzureNative.Network.Inputs.PublicIPAddressArgs {
+                    Id = publicIp.Id,
+                },
+            }
+        }
+    });
+
+    // Define a script to be run when the VM starts up.
+    var initScript = $@"#!/bin/bash
+        echo '<!DOCTYPE html>
+        <html lang=""en"">
+        <head>
+            <meta charset=""utf-8"">
+            <title>Hello, world!</title>
+        </head>
+        <body>
+            <h1>Hello, world! 👋</h1>
+            <p>Deployed with 💜 by <a href=""https://pulumi.com/"">Pulumi</a>.</p>
+        </body>
+        </html>' > index.html
+        sudo python3 -m http.server {servicePort} &";
+
+    // Create the virtual machine.
+    var vm = new AzureNative.Compute.VirtualMachine("vm", new()
+    {
+        ResourceGroupName = resourceGroup.Name,
+        NetworkProfile = new AzureNative.Compute.Inputs.NetworkProfileArgs {
+            NetworkInterfaces = new[] {
+                new AzureNative.Compute.Inputs.NetworkInterfaceReferenceArgs {
+                    Id = networkInterface.Id,
+                    Primary = true,
+                },
+            },
+        },
+        HardwareProfile = new AzureNative.Compute.Inputs.HardwareProfileArgs {
+            VmSize = vmSize,
+        },
+        OsProfile = new AzureNative.Compute.Inputs.OSProfileArgs {
+            ComputerName = vmName,
+            AdminUsername = adminUsername,
+            CustomData = Convert.ToBase64String(Encoding.UTF8.GetBytes(initScript)),
+            LinuxConfiguration = new AzureNative.Compute.Inputs.LinuxConfigurationArgs {
+                DisablePasswordAuthentication = true,
+                Ssh = new AzureNative.Compute.Inputs.SshConfigurationArgs {
+                    PublicKeys = new[]
+                    {
+                        new AzureNative.Compute.Inputs.SshPublicKeyArgs {
+                            KeyData = sshPublicKey,
+                            Path = $"/home/{adminUsername}/.ssh/authorized_keys",
+                        },
+                    },
+                },
+            },
+        },
+        StorageProfile = new AzureNative.Compute.Inputs.StorageProfileArgs {
+            OsDisk = new AzureNative.Compute.Inputs.OSDiskArgs {
+                Name = $"{vmName}-osdisk",
+                CreateOption = AzureNative.Compute.DiskCreateOptionTypes.FromImage,
+            },
+            ImageReference = new AzureNative.Compute.Inputs.ImageReferenceArgs {
+                Publisher = osImagePublisher,
+                Offer = osImageOffer,
+                Sku = osImageSku,
+                Version = osImageVersion,
+            },
+        },
+    });
+
+    // Once the machine is created, fetch its IP address and DNS hostname.
+    var vmAddress = vm.Id.Apply(_ => {
+        return AzureNative.Network.GetPublicIPAddress.Invoke(new()
+        {
+            ResourceGroupName = resourceGroup.Name,
+            PublicIpAddressName = publicIp.Name,
+        });
+    });
+
+    // Export the VM's hostname, public IP address, and HTTP URL.
+    return new Dictionary<string, object?>
+    {
+        ["hostname"] = vmAddress.Apply(addr => addr.DnsSettings!.Fqdn),
+        ["ip"] = vmAddress.Apply(addr => addr.IpAddress),
+        ["url"] = vmAddress.Apply(addr => $"http://{addr.DnsSettings!.Fqdn}:{servicePort}"),
+    };
+});
---
+++ vm-azure-typescript/index.ts
@@ -0,0 +1,158 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as network from "@pulumi/azure-native/network";
+import * as compute from "@pulumi/azure-native/compute";
+import * as random from "@pulumi/random";
+
+// Import the program's configuration settings.
+const config = new pulumi.Config();
+const vmName = config.get("vmName") || "my-server";
+const vmSize = config.get("vmSize") || "Standard_A1_v2";
+const osImage = config.get("osImage") || "Debian:debian-11:11:latest";
+const adminUsername = config.get("adminUsername") || "pulumiuser";
+const servicePort = config.get("servicePort") || "80";
+const sshPublicKey = config.require("sshPublicKey");
+
+const [ osImagePublisher, osImageOffer, osImageSku, osImageVersion ] = osImage.split(":");
+
+// Create a resource group.
+const resourceGroup = new resources.ResourceGroup("resource-group");
+
+// Create a virtual network.
+const virtualNetwork = new network.VirtualNetwork("network", {
+    resourceGroupName: resourceGroup.name,
+    addressSpace: {
+        addressPrefixes: ["10.0.0.0/16"],
+    },
+    subnets: [
+        {
+            name: `${vmName}-subnet`,
+            addressPrefix: "10.0.1.0/24",
+        },
+    ],
+});
+
+// Use a random string to give the VM a unique DNS name.
+var domainNameLabel = new random.RandomString("domain-label", {
+    length: 8,
+    upper: false,
+    special: false,
+}).result.apply(result => `${vmName}-${result}`);
+
+// Create a public IP address for the VM.
+const publicIp = new network.PublicIPAddress("public-ip", {
+    resourceGroupName: resourceGroup.name,
+    publicIPAllocationMethod: network.IPAllocationMethod.Dynamic,
+    dnsSettings: {
+        domainNameLabel: domainNameLabel,
+    },
+});
+
+// Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH).
+const securityGroup = new network.NetworkSecurityGroup("security-group", {
+    resourceGroupName: resourceGroup.name,
+    securityRules: [
+        {
+            name: `${vmName}-securityrule`,
+            priority: 1000,
+            direction: network.AccessRuleDirection.Inbound,
+            access: "Allow",
+            protocol: "Tcp",
+            sourcePortRange: "*",
+            sourceAddressPrefix: "*",
+            destinationAddressPrefix: "*",
+            destinationPortRanges: [
+                servicePort,
+                "22",
+            ],
+        }
+    ]
+});
+
+// Create a network interface with the virtual network, IP address, and security group.
+const networkInterface = new network.NetworkInterface("network-interface", {
+    resourceGroupName: resourceGroup.name,
+    networkSecurityGroup: {
+        id: securityGroup.id,
+    },
+    ipConfigurations: [{
+        name: `${vmName}-ipconfiguration`,
+        privateIPAllocationMethod: network.IPAllocationMethod.Dynamic,
+        subnet: {
+            id: virtualNetwork.subnets.apply(subnets => subnets![0].id!),
+        },
+        publicIPAddress: {
+            id: publicIp.id,
+        },
+    }],
+});
+
+// Define a script to be run when the VM starts up.
+const initScript = `#!/bin/bash
+    echo '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello, world!</title>
+    </head>
+    <body>
+        <h1>Hello, world! 👋</h1>
+        <p>Deployed with 💜 by <a href="https://pulumi.com/">Pulumi</a>.</p>
+    </body>
+    </html>' > index.html
+    sudo python3 -m http.server ${servicePort} &`;
+
+// Create the virtual machine.
+const vm = new compute.VirtualMachine("vm", {
+    resourceGroupName: resourceGroup.name,
+    networkProfile: {
+        networkInterfaces: [
+            {
+                id: networkInterface.id,
+                primary: true,
+            },
+        ],
+    },
+    hardwareProfile: {
+        vmSize: vmSize,
+    },
+    osProfile: {
+        computerName: vmName,
+        adminUsername: adminUsername,
+        customData: Buffer.from(initScript).toString("base64"),
+        linuxConfiguration: {
+            disablePasswordAuthentication: true,
+            ssh: {
+                publicKeys: [
+                    {
+                        keyData: sshPublicKey,
+                        path: `/home/${adminUsername}/.ssh/authorized_keys`,
+                    },
+                ],
+            },
+        },
+    },
+    storageProfile: {
+        osDisk: {
+            name: `${vmName}-osdisk`,
+            createOption: compute.DiskCreateOption.FromImage,
+        },
+        imageReference: {
+            publisher: osImagePublisher,
+            offer: osImageOffer,
+            sku: osImageSku,
+            version: osImageVersion,
+        },
+    },
+});
+
+// Once the machine is created, fetch its IP address and DNS hostname.
+const vmAddress = vm.id.apply(_ => network.getPublicIPAddressOutput({
+    resourceGroupName: resourceGroup.name,
+    publicIpAddressName: publicIp.name,
+}));
+
+// Export the VM's hostname, public IP address, and HTTP URL.
+export const ip = vmAddress.ipAddress;
+export const hostname = vmAddress.dnsSettings?.apply(settings => settings?.fqdn);
+export const url = hostname?.apply(name => `http://${name}:${servicePort}`);
