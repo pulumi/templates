1b733fc6ec687057ed905b693a4ac1266d72aa8f
Dewan Ishtiaque Ahmed <dewan.ahmed@aiven.io>
2023-01-12 14:13:29 -0400

    Updates based on review.

--- aiven-typescript/index.ts
+++ aiven-typescript/index.ts
@@ -1,26 +1,22 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aiven from "@pulumi/aiven";
 
+// Import the program's configuration settings.
+const config = new pulumi.Config();
+const project = config.require("project");
+const cloudName = config.require("cloudName");
+const plan = config.require("plan");
+const serviceName = config.require("serviceName");
+
 // Create a Kafka service.
 const kafka = new aiven.Kafka("kafka", {
-    project: "<YOUR_AIVEN_PROJECT_NAME>",
-    cloudName: "google-europe-west1",
-    plan: "business-4",
-    serviceName: "kafka-gcp-eu",
-    maintenanceWindowDow: "monday",
-    maintenanceWindowTime: "10:00:00",
-    kafkaUserConfig: {
-        kafkaRest: "true",
-        kafkaConnect: "true",
-        schemaRegistry: "true",
-        kafkaVersion: "3.2",
-        kafka: {
-            groupMaxSessionTimeoutMs: "70000",
-            logRetentionBytes: "1000000000",
-        },
-        publicAccess: {
-            kafkaRest: "true",
-            kafkaConnect: "true",
-        },
-    },
+    project: project,
+    cloudName: cloudName,
+    plan: plan,
+    serviceName: serviceName,
 });
+
+// Export the service host, port, and URI.
+export const serviceHost = kafka.serviceHost;
+export const servicePort = kafka.servicePort;
+export const serviceURI = kafka.serviceURI;
