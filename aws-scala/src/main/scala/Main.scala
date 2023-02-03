6f0fdf73ac51a6c982a1207dc6e88b68af05fc82
Asaf Levy <asaf400@gmail.com>
2023-02-03 12:33:53 +0200

    Update aws-scala/src/main/scala/Main.scala
    Yes indeed, Comments are important
    
    Co-authored-by: Christian Nunciato <christian@pulumi.com>

--- aws-scala/src/main/scala/Main.scala
+++ aws-scala/src/main/scala/Main.scala
@@ -6,7 +6,11 @@ import com.pulumi.aws.s3.Bucket
 object App {
   def main(args: Array[String]): Unit = {
     Pulumi.run { (ctx: Context) =>
+
+      // Create an AWS resource (S3 Bucket)
       var bucket = new Bucket("my-bucket");
+
+      // Export the name of the bucket
       ctx.`export`("bucketName", bucket.bucket())
     }
   }
