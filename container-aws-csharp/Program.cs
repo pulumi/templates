556c4941d67d74029593663de9645b8c1aa979c7
Christian Nunciato <c@nunciato.org>
2023-02-17 15:10:31 -0800

    Force-delete repositories in container-aws templates

--- container-aws-csharp/Program.cs
+++ container-aws-csharp/Program.cs
@@ -3,7 +3,7 @@ using Pulumi;
 using Aws = Pulumi.Aws;
 using Awsx = Pulumi.Awsx;
 
-return await Deployment.RunAsync(() => 
+return await Deployment.RunAsync(() =>
 {
     var config = new Config();
     var containerPort = config.GetInt32("containerPort") ?? 80;
@@ -13,7 +13,10 @@ return await Deployment.RunAsync(() =>
 
     var loadbalancer = new Awsx.Lb.ApplicationLoadBalancer("loadbalancer");
 
-    var repo = new Awsx.Ecr.Repository("repo");
+    var repo = new Awsx.Ecr.Repository("repo", new()
+    {
+        ForceDelete = true,
+    });
 
     var image = new Awsx.Ecr.Image("image", new()
     {
@@ -50,4 +53,3 @@ return await Deployment.RunAsync(() =>
         ["url"] = loadbalancer.LoadBalancer.Apply(loadBalancer => Output.Format($"http://{loadBalancer.DnsName}")),
     };
 });
-
