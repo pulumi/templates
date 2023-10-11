188a52ae8f51c16793f964209b5598f276082266
Guinevere Saenger <guinevere@pulumi.com>
2023-10-11 11:39:16 -0700

    Update all Go templates

--- vm-gcp-go/main.go
+++ vm-gcp-go/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp/compute"
+	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/compute"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
