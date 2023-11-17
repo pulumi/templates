0c373136041c94b1433fa6003de5e9a8ca556545
Scott S. Lowe <slowe@pulumi.com>
2023-11-17 08:56:53 -0700

    Add SSH key to enable testing (#683)
    Updates templates to add an SSH key resource
    Uses the SSH key resource for the VM resource
    Removes need for SSH key in configuration to enable testing
    
    Signed-off-by: Scott Lowe <slowe@pulumi.com>

--- vm-azure-typescript/index.ts
+++ vm-azure-typescript/index.ts
@@ -3,22 +3,28 @@ import * as resources from "@pulumi/azure-native/resources";
 import * as network from "@pulumi/azure-native/network";
 import * as compute from "@pulumi/azure-native/compute";
 import * as random from "@pulumi/random";
+import * as tls from "@pulumi/tls";
 
-// Import the program's configuration settings.
+// Import the program's configuration settings
 const config = new pulumi.Config();
 const vmName = config.get("vmName") || "my-server";
 const vmSize = config.get("vmSize") || "Standard_A1_v2";
 const osImage = config.get("osImage") || "Debian:debian-11:11:latest";
 const adminUsername = config.get("adminUsername") || "pulumiuser";
 const servicePort = config.get("servicePort") || "80";
-const sshPublicKey = config.require("sshPublicKey");
 
 const [ osImagePublisher, osImageOffer, osImageSku, osImageVersion ] = osImage.split(":");
 
-// Create a resource group.
+// Create an SSH key
+const sshKey = new tls.PrivateKey("ssh-key", {
+    algorithm: "RSA",
+    rsaBits: 4096,
+});
+
+// Create a resource group
 const resourceGroup = new resources.ResourceGroup("resource-group");
 
-// Create a virtual network.
+// Create a virtual network
 const virtualNetwork = new network.VirtualNetwork("network", {
     resourceGroupName: resourceGroup.name,
     addressSpace: {
@@ -32,14 +38,14 @@ const virtualNetwork = new network.VirtualNetwork("network", {
     ],
 });
 
-// Use a random string to give the VM a unique DNS name.
+// Use a random string to give the VM a unique DNS name
 var domainNameLabel = new random.RandomString("domain-label", {
     length: 8,
     upper: false,
     special: false,
 }).result.apply(result => `${vmName}-${result}`);
 
-// Create a public IP address for the VM.
+// Create a public IP address for the VM
 const publicIp = new network.PublicIPAddress("public-ip", {
     resourceGroupName: resourceGroup.name,
     publicIPAllocationMethod: network.IPAllocationMethod.Dynamic,
@@ -48,7 +54,7 @@ const publicIp = new network.PublicIPAddress("public-ip", {
     },
 });
 
-// Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH).
+// Create a security group allowing inbound access over ports 80 (for HTTP) and 22 (for SSH)
 const securityGroup = new network.NetworkSecurityGroup("security-group", {
     resourceGroupName: resourceGroup.name,
     securityRules: [
@@ -69,7 +75,7 @@ const securityGroup = new network.NetworkSecurityGroup("security-group", {
     ]
 });
 
-// Create a network interface with the virtual network, IP address, and security group.
+// Create a network interface with the virtual network, IP address, and security group
 const networkInterface = new network.NetworkInterface("network-interface", {
     resourceGroupName: resourceGroup.name,
     networkSecurityGroup: {
@@ -87,7 +93,7 @@ const networkInterface = new network.NetworkInterface("network-interface", {
     }],
 });
 
-// Define a script to be run when the VM starts up.
+// Define a script to be run when the VM starts up
 const initScript = `#!/bin/bash
     echo '<!DOCTYPE html>
     <html lang="en">
@@ -102,7 +108,7 @@ const initScript = `#!/bin/bash
     </html>' > index.html
     sudo python3 -m http.server ${servicePort} &`;
 
-// Create the virtual machine.
+// Create the virtual machine
 const vm = new compute.VirtualMachine("vm", {
     resourceGroupName: resourceGroup.name,
     networkProfile: {
@@ -125,7 +131,7 @@ const vm = new compute.VirtualMachine("vm", {
             ssh: {
                 publicKeys: [
                     {
-                        keyData: sshPublicKey,
+                        keyData: sshKey.publicKeyOpenssh,
                         path: `/home/${adminUsername}/.ssh/authorized_keys`,
                     },
                 ],
@@ -146,13 +152,14 @@ const vm = new compute.VirtualMachine("vm", {
     },
 });
 
-// Once the machine is created, fetch its IP address and DNS hostname.
+// Once the machine is created, fetch its IP address and DNS hostname
 const vmAddress = vm.id.apply(_ => network.getPublicIPAddressOutput({
     resourceGroupName: resourceGroup.name,
     publicIpAddressName: publicIp.name,
 }));
 
-// Export the VM's hostname, public IP address, and HTTP URL.
+// Export the VM's hostname, public IP address, HTTP URL, and SSH private key
 export const ip = vmAddress.ipAddress;
 export const hostname = vmAddress.dnsSettings?.apply(settings => settings?.fqdn);
 export const url = hostname?.apply(name => `http://${name}:${servicePort}`);
+export const privatekey = sshKey.privateKeyOpenssh;
