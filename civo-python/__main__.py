44216eb395722bf1b7c144fe7e2ff5a972a11f2d
user.email <julien@caffeine.lu>
2024-08-20 15:37:22 +0200

    Update more templates to used dictionary literals

--- civo-python/__main__.py
+++ civo-python/__main__.py
@@ -1,17 +1,17 @@
 import pulumi
 import pulumi_civo as civo
 
-firewall = civo.Firewall("civo-firewall",
-                         create_default_rules=True,
-                         region='LON1')
+firewall = civo.Firewall("civo-firewall", create_default_rules=True, region="LON1")
 
-cluster = civo.KubernetesCluster('civo-k3s-cluster',
-                                 name='myFirstCivoCluster',
-                                 region='LON1',
-                                 firewall_id=firewall.id,
-                                 pools=civo.KubernetesClusterPoolsArgs(
-                                     node_count=3,
-                                     size="g4s.kube.medium",
-                                 ))
+cluster = civo.KubernetesCluster(
+    "civo-k3s-cluster",
+    name="myFirstCivoCluster",
+    region="LON1",
+    firewall_id=firewall.id,
+    pools={
+        "node_count": 3,
+        "size": "g4s.kube.medium",
+    },
+)
 
-pulumi.export('cluster_name', cluster.name)
+pulumi.export("cluster_name", cluster.name)
--- container-aws-python/__main__.py
+++ container-aws-python/__main__.py
@@ -14,35 +14,34 @@ cluster = aws.ecs.Cluster("cluster")
 loadbalancer = awsx.lb.ApplicationLoadBalancer("loadbalancer")
 
 # An ECR repository to store our application's container image
-repo = awsx.ecr.Repository("repo", awsx.ecr.RepositoryArgs(
-    force_delete=True,
-))
+repo = awsx.ecr.Repository("repo", force_delete=True)
 
 # Build and publish our application's container image from ./app to the ECR repository
 image = awsx.ecr.Image(
-    "image",
-    repository_url=repo.url,
-    context="./app",
-    platform="linux/amd64")
+    "image", repository_url=repo.url, context="./app", platform="linux/amd64"
+)
 
 # Deploy an ECS Service on Fargate to host the application container
 service = awsx.ecs.FargateService(
     "service",
     cluster=cluster.arn,
     assign_public_ip=True,
-    task_definition_args=awsx.ecs.FargateServiceTaskDefinitionArgs(
-        container=awsx.ecs.TaskDefinitionContainerDefinitionArgs(
-            name="app",
-            image=image.image_uri,
-            cpu=cpu,
-            memory=memory,
-            essential=True,
-            port_mappings=[awsx.ecs.TaskDefinitionPortMappingArgs(
-                container_port=container_port,
-                target_group=loadbalancer.default_target_group,
-            )],
-        ),
-    ))
+    task_definition_args={
+        "container": {
+            "name": "app",
+            "image": image.image_uri,
+            "cpu": cpu,
+            "memory": memory,
+            "essential": True,
+            "port_mappings": [
+                {
+                    "container_port": container_port,
+                    "target_group": loadbalancer.default_target_group,
+                }
+            ],
+        },
+    },
+)
 
 # The URL at which the container's HTTP endpoint will be available
 export("url", Output.concat("http://", loadbalancer.load_balancer.dns_name))
