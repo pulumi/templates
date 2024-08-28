30b2c7f92f98cd02cfee1b924a5ebc93d1bb24bf
user.email <julien@caffeine.lu>
2024-08-28 11:36:18 +0200

    Update more python templates to use dictionary literals
    With type checking for dictionary literals, we now prefer this style over args classes https://github.com/pulumi/pulumi/issues/12689

--- pinecone-python/__main__.py
+++ pinecone-python/__main__.py
@@ -3,14 +3,16 @@
 import pulumi
 import pinecone_pulumi as pinecone
 
-my_pinecone_index = pinecone.PineconeIndex("myPineconeIndex",
-                                           name="example-index-python",
-                                           metric=pinecone.IndexMetric.COSINE,
-                                           spec=pinecone.PineconeSpecArgs(
-                                               serverless=pinecone.PineconeServerlessSpecArgs(
-                                                   cloud=pinecone.ServerlessSpecCloud.AWS,
-                                                   region="us-west-2",
-                                               ),
-                                           ))
+my_pinecone_index = pinecone.PineconeIndex(
+    "myPineconeIndex",
+    name="example-index-python",
+    metric=pinecone.IndexMetric.COSINE,
+    spec={
+        "serverless": {
+            "cloud": pinecone.ServerlessSpecCloud.AWS,
+            "region": "us-west-2",
+        },
+    },
+)
 
 pulumi.export("host", my_pinecone_index.host)
