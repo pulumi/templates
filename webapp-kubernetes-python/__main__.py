45e5f8a96e05b13513d280db20046868c91b98e4
Julien Poissonnier <julien@caffeine.lu>
2024-08-30 11:21:02 +0200

    Update more python templates to use dictionary literals
    With type checking for dictionary literals, we now prefer this style over args classes https://github.com/pulumi/pulumi/issues/12689

--- webapp-kubernetes-python/__main__.py
+++ webapp-kubernetes-python/__main__.py
@@ -12,17 +12,17 @@ app_labels = {
 # Create a namespace
 webserverns = kubernetes.core.v1.Namespace(
     "webserver",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        name=k8sNamespace,
-    )
+    metadata={
+        "name": k8sNamespace,
+    },
 )
 
 # Create a ConfigMap for the Nginx configuration
 webserverconfig = kubernetes.core.v1.ConfigMap(
     "webserverconfig",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
-    ),
+    metadata={
+        "namespace": webserverns.metadata.name,
+    },
     data={
         "nginx.conf": """events { }
 http {
@@ -37,64 +37,74 @@ http {
   }
 }
 """,
-    }
+    },
 )
 
 # Create a Deployment with a user-defined number of replicas
 webserverdeployment = kubernetes.apps.v1.Deployment(
     "webserverdeployment",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
-    ),
-    spec=kubernetes.apps.v1.DeploymentSpecArgs(
-        selector=kubernetes.meta.v1.LabelSelectorArgs(
-            match_labels=app_labels,
-        ),
-        replicas=numReplicas,
-        template=kubernetes.core.v1.PodTemplateSpecArgs(
-            metadata=kubernetes.meta.v1.ObjectMetaArgs(
-                labels=app_labels,
-            ),
-            spec=kubernetes.core.v1.PodSpecArgs(
-                containers=[kubernetes.core.v1.ContainerArgs(
-                    image="nginx",
-                    name="nginx",
-                    volume_mounts=[kubernetes.core.v1.VolumeMountArgs(
-                        mount_path="/etc/nginx/nginx.conf",
-                        name="nginx-conf-volume",
-                        read_only=True,
-                        sub_path="nginx.conf",
-                    )],
-                )],
-                volumes=[kubernetes.core.v1.VolumeArgs(
-                    config_map=kubernetes.core.v1.ConfigMapVolumeSourceArgs(
-                        items=[kubernetes.core.v1.KeyToPathArgs(
-                            key="nginx.conf",
-                            path="nginx.conf",
-                        )],
-                        name=webserverconfig.metadata.name,
-                    ),
-                    name="nginx-conf-volume",
-                )],
-            ),
-        ),
-    )
+    metadata={
+        "namespace": webserverns.metadata.name,
+    },
+    spec={
+        "selector": {
+            "match_labels": app_labels,
+        },
+        "replicas": numReplicas,
+        "template": {
+            "metadata": {
+                "labels": app_labels,
+            },
+            "spec": {
+                "containers": [
+                    {
+                        "image": "nginx",
+                        "name": "nginx",
+                        "volume_mounts": [
+                            {
+                                "mount_path": "/etc/nginx/nginx.conf",
+                                "name": "nginx-conf-volume",
+                                "read_only": True,
+                                "sub_path": "nginx.conf",
+                            }
+                        ],
+                    }
+                ],
+                "volumes": [
+                    {
+                        "config_map": {
+                            "items": [
+                                {
+                                    "key": "nginx.conf",
+                                    "path": "nginx.conf",
+                                }
+                            ],
+                            "name": webserverconfig.metadata.name,
+                        },
+                        "name": "nginx-conf-volume",
+                    }
+                ],
+            },
+        },
+    },
 )
 
 # Expose the Deployment as a Kubernetes Service
 webserverservice = kubernetes.core.v1.Service(
     "webserverservice",
-    metadata=kubernetes.meta.v1.ObjectMetaArgs(
-        namespace=webserverns.metadata.name,
-    ),
-    spec=kubernetes.core.v1.ServiceSpecArgs(
-        ports=[kubernetes.core.v1.ServicePortArgs(
-            port=80,
-            target_port=80,
-            protocol="TCP",
-        )],
-        selector=app_labels,
-    )
+    metadata={
+        "namespace": webserverns.metadata.name,
+    },
+    spec={
+        "ports": [
+            {
+                "port": 80,
+                "target_port": 80,
+                "protocol": "TCP",
+            }
+        ],
+        "selector": app_labels,
+    },
 )
 
 # Export some values for use elsewhere
