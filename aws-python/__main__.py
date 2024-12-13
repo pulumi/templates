d61654329280dc2264551ffe39fc65ec8ffefef9
Justin Van Patten <jvp@justinvp.com>
2024-12-13 07:12:25 -0800

    Add unit tests to aws-python

--- aws-python/__main__.py
+++ aws-python/__main__.py
@@ -1,10 +1,6 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-from pulumi_aws import s3
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.BucketV2('my-bucket')
+import infra
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+# Export the name of the bucket.
+pulumi.export("bucket_name", infra.bucket.id)
