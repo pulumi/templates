819109b2cb4c089cc8cc873702c82cc6a2bdf98f
Justin Van Patten <jvp@justinvp.com>
2024-12-13 08:35:12 -0800

    Add unit tests to aws-typescript

--- aws-typescript/index.ts
+++ aws-typescript/index.ts
@@ -1,9 +1,4 @@
-import * as pulumi from "@pulumi/pulumi";
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
+import * as infra from "./infra";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
+// Export the name of the bucket.
+export const bucketName = infra.bucket.id;
