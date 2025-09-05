988a235ec9dc4d7beb1f43aa11f46defaf7461ef
Divya <divya@garden.co>
2025-09-05 11:56:49 -0500

    fix: check fields

--- vultr-typescript/index.ts
+++ vultr-typescript/index.ts
@@ -3,11 +3,18 @@ import * as vultr from "@ediri/vultr";
 
 const config = new pulumi.Config();
 const instanceName = config.get("instanceName") || "my-instance";
-const planName = config.get("plan") || "vc2-1c-1gb";
+const plan = config.get("plan") || "vc2-1c-1gb";
 const region = config.get("region") || "ewr";
-const hostName = config.get("hostName") || "my-instance-hostname";
+const hostname = config.get("hostName") || "my-instance-hostname";
 const tags = config.get("tags") ? config.get("tags")!.split(",") : ["web", "dev"];
 const label = config.get("label") || "my-label";
+
+// Create a new instance:
+const myInstance = new vultr.Instance(instanceName, {
+    osId: 1743,
+    plan,
+    region,
+});
 
 // Create a new instance with options:
 const vultrInstance = new vultr.Instance(instanceName, {
@@ -19,10 +26,10 @@ const vultrInstance = new vultr.Instance(instanceName, {
     ddosProtection: true,
     disablePublicIpv4: true,
     enableIpv6: true,
-    hostName,
+    hostname,
     label,
     osId: 1743, // Ubuntu 22.04 LTS x64
-    planName,
+    plan,
     region,
     tags,
 });
