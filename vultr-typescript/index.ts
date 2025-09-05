7a651938792cb020fa3341ebe720647e86fc9e1d
Divya <divya@garden.co>
2025-09-05 11:47:53 -0500

    Contribute Vultr template

---
+++ vultr-typescript/index.ts
@@ -0,0 +1,32 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as vultr from "@ediri/vultr";
+
+const config = new pulumi.Config();
+const instanceName = config.get("instanceName") || "my-instance";
+const planName = config.get("plan") || "vc2-1c-1gb";
+const region = config.get("region") || "ewr";
+const hostName = config.get("hostName") || "my-instance-hostname";
+const tags = config.get("tags") ? config.get("tags")!.split(",") : ["web", "dev"];
+const label = config.get("label") || "my-label";
+
+// Create a new instance with options:
+const vultrInstance = new vultr.Instance(instanceName, {
+    activationEmail: false,
+    backups: "enabled",
+    backupsSchedule: {
+        type: "daily",
+    },
+    ddosProtection: true,
+    disablePublicIpv4: true,
+    enableIpv6: true,
+    hostName,
+    label,
+    osId: 1743, // Ubuntu 22.04 LTS x64
+    planName,
+    region,
+    tags,
+});
+
+export const instanceId = vultrInstance.id;
+export const instanceStatus = vultrInstance.status;
+export const mainIp = vultrInstance.mainIp;
