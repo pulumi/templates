7f664ac8b185863e8ae6c599a8ef23a17504d9ea
Zaid Ajaj <zaid.naom@gmail.com>
2025-09-19 03:27:50 +0200

    Update GCP sdks to v9

--- kubernetes-gcp-go/go.mod
+++ kubernetes-gcp-go/go.mod
@@ -3,6 +3,6 @@ module ${PROJECT}
 go 1.20
 
 require (
-	github.com/pulumi/pulumi-gcp/sdk/v8 v8.0.0
+	github.com/pulumi/pulumi-gcp/sdk/v9 v9.0.0
 	github.com/pulumi/pulumi/sdk/v3 v3.91.1
 )
--- vm-gcp-go/main.go
+++ vm-gcp-go/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	"github.com/pulumi/pulumi-gcp/sdk/v8/go/gcp/compute"
+	"github.com/pulumi/pulumi-gcp/sdk/v9/go/gcp/compute"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
