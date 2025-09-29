ccf292df1a8709c78929ff62b3dc244b487e623c
Ian Wahbe <me@iwahbe.com>
2025-09-29 13:44:43 +0200

    Fix the Go template generator

--- generator/generate-go-mod.sh
+++ generator/generate-go-mod.sh
@@ -4,6 +4,8 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+GO_VERSION="1.23"
+
 # Fetch the latest release version of the given Pulumi repo.
 fetch_latest_version() {
 	local repo="pulumi/$1"
@@ -18,7 +20,7 @@ PROVIDER_LIST="aiven,alicloud,auth0,aws,azure,azure-classic,civo,digitalocean,eq
 IFS=',' read -ra PROVIDERS <<<"$PROVIDER_LIST"
 
 for i in "${PROVIDERS[@]}"; do
-	echo "Updating" $i "template"
+	echo "Updating $i template"
 	if [ "$i" = "azure-classic" ]; then
 		PROVIDER_NAME="azure"
 	elif [ "$i" = "azure" ]; then
@@ -27,10 +29,34 @@ for i in "${PROVIDERS[@]}"; do
 		PROVIDER_NAME="$i"
 	fi
 	PROVIDER_VERSION="$(fetch_latest_version "pulumi-$PROVIDER_NAME")"
-	sed -e "s/\${VERSION}/$PROVIDER_VERSION/g" -e "s/\${PULUMI_VERSION}/$PULUMI_VERSION/g" mod-templates/$i-template.txt | tee ../$i-go/go.mod
-	echo "Updated $i go mod template to be Pulumi $PULUMI_VERSION and pulumi-$i to be" $PROVIDER_VERSION
+	MAJOR_VERSION=""
+	if [[ "$PROVIDER_VERSION" =~ ^v([0-9]+)\. ]]; then
+		major_num="${BASH_REMATCH[1]}"
+		if [ "$major_num" -gt 1 ]; then
+			MAJOR_VERSION="/v$major_num"
+		fi
+	fi
+        cat<<EOF > "../$i-go/go.mod"
+module \${PROJECT}
+
+go ${GO_VERSION}
+
+require (
+	github.com/pulumi/pulumi-${PROVIDER_NAME}/sdk${MAJOR_VERSION} ${PROVIDER_VERSION}
+	github.com/pulumi/pulumi/sdk/v3 ${PULUMI_VERSION}
+)
+EOF
+	echo "Updated $i go mod template to be Pulumi $PULUMI_VERSION and pulumi-$i to be $PROVIDER_VERSION"
 done
 
 echo "Updating go template"
-sed -e "s/\${PULUMI_VERSION}/$PULUMI_VERSION/g" mod-templates/go-template.txt | tee ../go/go.mod
+cat<<EOF > ../go/go.mod
+module \${PROJECT}
+
+go ${GO_VERSION}
+
+require (
+	github.com/pulumi/pulumi/sdk/v3 ${PULUMI_VERSION}
+)
+EOF
 echo "Updated go template to be Pulumi $PULUMI_VERSION"
