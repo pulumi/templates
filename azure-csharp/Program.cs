9e81f2cf54d3c94b95ac95633d509a38ab8c470f
Cam Soper <cam.soper@outlook.com>
2025-12-05 19:41:33 -0600

    Fix namespace

--- azure-csharp/Program.cs
+++ azure-csharp/Program.cs
@@ -2,6 +2,7 @@
 using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
