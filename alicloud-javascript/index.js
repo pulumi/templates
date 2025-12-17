e4afaa6d183dd4739943fbbe29ad435314a87270
Josh Kodroff <josh@pulumi.com>
2025-12-16 22:20:35 -0500

    Remove JavaScript templates
    Although it is possible to write Pulumi in JavaScript, we do not
    maintain JS examples - only TypeScript. This commit removes all of our
    JS templates except for the base `javascript` and also modifies the
    README to remove a reference to JavaScript.

--- alicloud-javascript/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");
-const alicloud = require("@pulumi/alicloud");
-
-// Create an AliCloud resource (OSS Bucket)
-const bucket = new alicloud.oss.Bucket("my-bucket");
-
-// Export the name of the bucket
-exports.bucketName = bucket.id;
--- github-javascript/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const pulumi = require("@pulumi/pulumi");
-const github = require("@pulumi/github")
-
-const repo = new github.Repository("demo-repo", {
-    description: "Demo Repository for GitHub",
-});
-
-exports.repositoryName = repo.name
